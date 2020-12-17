2d9dfb9ef7d6c6da1af2ee501efa614a1328a6c7
xiang5282 <60284309+xiang5282@users.noreply.github.com>
2020-12-18 00:57:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,8 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/kenzok8/small.git' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/kenzok8/small.git' feeds.conf.default
